909249401fc4c812f73d6e2137edd83be6e009b1
David Poole <davpoole@iastate.edu>
2020-12-16 13:47:34 -0600

    Add command line arguments to sad_guess.cpp

--- sad_guess/sad_guess.cpp
+++ sad_guess/sad_guess.cpp
@@ -12,12 +12,12 @@
 // basis set information from JuliaChem.jl's bsed.h5 basis set      //
 // database. Coefficients will be arriving soon.                    //
 //------------------------------------------------------------------//
-int main() {
+int main(int argc, char* argv[]) {
   //-------------------------------------//
   //-- change atom/basis set pair here --//
   //-------------------------------------//
-  std::string atom = "O";
-  std::string basis = "PCSeg-0";
+  std::string atom(argv[1]) ;
+  std::string basis(argv[2]);
 
   //-- go into primary atom-basis set pair group --//
   H5::H5File file("sadgss.h5", H5F_ACC_RDONLY);
