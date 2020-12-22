cbaa031e15e77be78ea538147a63bca3f4d87de3
davpoolechem <davpoole@iastate.edu>
2020-12-22 09:27:09 -0600

    Add symmetry test to psi4_compare

--- debug/psi4_compare.jl
+++ debug/psi4_compare.jl
@@ -4,6 +4,9 @@ psi4 = pyimport("psi4")
 
 using HDF5
 
+#== globals ==#
+const THRESHOLD = 0.00001
+
 #== psi4 setup ==# 
 psi4.set_memory("500 MB")
 psi4.core.set_output_file("psi4_compare.dat")
@@ -46,16 +49,41 @@ close(debug)
 
 #== comparison ==#
 S_diff = S_standalone .- S_psi4
-#display(S_standalone[:])
-for i in 1:length(S_diff)
-  if S_diff[i] > 0.00001
-    println(i," ", S_standalone[i]," ",  S_psi4[i]," ",  S_diff[i])
+diff_array = []
+
+println("Comparing Psi4 overlap to standalone overlap...")
+for i in 1:size(S_diff)[1], j in 1:size(S_diff)[2]
+  if S_diff[i,j] > THRESHOLD 
+    println((i,j), ": ", S_standalone[i,j], ", ", S_psi4[i,j], ", ", 
+      S_diff[i,j])
+    push!(diff_array,(i,j))
   end
 end
+println()
 
-leng = Int64(sqrt(length(S_diff)))
-
-for i in 0:(leng-1)
-  index = (leng+1)*i + 1
-  println(index, " ", S_standalone[index], " ", S_psi4[index], " ", S_diff[index])
+#== check for symmetry issues ==#
+println("Checking for symmetry issues in S_diff...")
+diff_symm_array = []
+for tuple in diff_array
+  if S_diff[tuple...] - S_diff[reverse(tuple)...] > THRESHOLD 
+    push!(diff_symm_array,tuple)
+    println(tuple, ": ", S_diff[tuple...], ", ", 
+      S_diff[reverse(tuple)...])
+  end
 end
+println()
+
+#== check diagonal elements ==#
+println("Printing values of diagonal elements...")
+
+#leng = Int64(sqrt(length(S_diff)))
+#for i in 0:(leng-1)
+#  index = (leng+1)*i + 1
+#  println(index, " ", S_standalone[index], " ", S_psi4[index], " ", S_diff[index])
+#end
+
+for index in 1:size(S_diff)[1]
+  println((index,index), ": ", S_standalone[index, index], ", ", 
+    S_psi4[index, index], ", ", S_diff[index, index])
+end
+println()
