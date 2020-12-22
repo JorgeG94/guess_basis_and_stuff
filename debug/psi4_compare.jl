bf1b7a3111a67d790c0d087f13a7d9844072ab5e
davpoolechem <davpoole@iastate.edu>
2020-12-21 20:57:12 -0600

    iUpdate psi4_compare scrip

--- debug/psi4_compare.jl
+++ debug/psi4_compare.jl
@@ -2,20 +2,23 @@
 using PyCall
 psi4 = pyimport("psi4")
 
+using HDF5
+
 #== psi4 setup ==# 
 psi4.set_memory("500 MB")
-psi4.core.set_output_file("w1.dat")
+psi4.core.set_output_file("psi4_compare.dat")
 
 geom = psi4.geometry("
-  O  -4.3997  1.0764  7.7009                                                   
-  H  -3.9597  0.2664  7.3109                                                   
-  H  -4.4297  0.9964  8.7009  
+  O      1.00339        0.03093        0.09503                                
+  O      2.32339        0.03093        0.09503                                
+  H      0.68006        -0.87208        -0.04962                              
+  H      2.64672        0.93395        0.23969                                 
   symmetry=c1
 ")
 
 options = Dict(
   "REFERENCE" => "RHF",
-  "BASIS" => "PCSEG-0", 
+  "BASIS" => "6-31G(d)", 
   "SCF_TYPE"  => "DIRECT",
   "GUESS" => "SAD",
   "SAD_SCF_TYPE" => "DIRECT",
@@ -29,6 +32,17 @@ psi4.set_options(options)
 name = "scf/"*options["BASIS"]
 scf_e, scf_wfn = psi4.energy(name, molecule=geom, return_wfn=true)
 
-S = scf_wfn.S().to_array()
-println("Overlap matrix:")
-display(S); println()
+S_psi4 = scf_wfn.S().to_array()
+
+#== standalone setup ==#
+debug = h5open("debug.h5", "r")
+
+tmp = debug["RHF"]["Iteration-None"]["S"][:]
+S_standalone = reshape(tmp,(Int64(sqrt(length(tmp))), Int64(sqrt(length(tmp)))))
+
+close(debug)
+
+#== comparison ==#
+S_diff = S_standalone .- S_psi4
+display(S_diff[:])
+
