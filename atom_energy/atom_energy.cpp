acddb23898d619a7aceb334971244cc5bb6248dc
David Poole <davpoole@iastate.edu>
2021-02-24 11:54:17 -0600

    Add new folder generating individual atom energies

---
+++ atom_energy/atom_energy.cpp
@@ -0,0 +1,61 @@
+#include <array>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <H5Cpp.h>
+
+//------------------------------------------------------------------//
+// This code loops over the shells of a specified atom with a given //
+// basis set and prints out the exponents for each shell, acquiring //
+// basis set information from JuliaChem.jl's bsed.h5 basis set      //
+// database. Coefficients will be arriving soon.                    //
+//------------------------------------------------------------------//
+int main(int argc, char* argv[]) {
+  //-------------------------------------//
+  //-- change atom/basis set pair here --//
+  //-------------------------------------//
+  std::string atom(argv[1]);
+  std::string basis(argv[2]);
+
+  //-- go into primary atom-basis set pair group --//
+  H5::H5File file("eatom.h5", H5F_ACC_RDONLY);
+
+  std::cout << "ATOM: " << atom << std::endl
+            << "BASIS: " << basis << std::endl
+            << std::endl;
+
+  std::string atom_basis_pair = atom + "/" + basis;
+  
+  H5::DataSet guess_dataset = file.openDataSet(atom_basis_pair);
+
+  H5::DataSpace guess_dataspace = guess_dataset.getSpace();
+  hsize_t guess_num_elements;
+  int guess_rank = guess_dataspace.getSimpleExtentDims(&guess_num_elements, NULL);
+  int nbas = floor(std::sqrt(guess_num_elements));
+
+  std::cout << guess_rank << ": " << guess_num_elements << std::endl;
+
+  //-- read from data set --//
+  std::vector<double> guess_buf(guess_num_elements, 0.0);
+  guess_dataset.read(guess_buf.data(), H5::PredType::NATIVE_DOUBLE);
+
+  //-- print results --//
+  std::cout << "ATOM " << atom << " / BASIS SET " << basis
+              << " SAD GUESS:" << std::endl
+              << "-----------------------------------------------------"
+              << std::endl 
+              << "-----------------------------------------------------"
+              << std::endl;
+  std::cout << "ELEMENT     |     GUESS     " << std::endl; 
+  std::cout << "-------     |     -----     " << std::endl; 
+ 
+  for (int i = 0, idx = 0; i != nbas; ++i) {
+    for (int j = 0; j != nbas; ++j, ++idx) {
+      std::cout << i << "   " << j << "   |     " << guess_buf.at(idx) << std::endl; 
+    }
+  }   
+  std::cout << std::endl;
+  return 0;
+}
---
+++ atom_energy/atom_energy.jl
@@ -0,0 +1,90 @@
+#== import modules here ==#
+using PyCall
+psi4 = pyimport("psi4")
+
+using HDF5
+#using MPI
+
+#== psi4 setup ==# 
+psi4.set_memory("500 MB")
+psi4.core.set_output_file("atom_energy.dat")
+
+#===============#
+#== MPI setup ==#
+#===============#
+#MPI.Init()
+#comm = MPI.COMM_WORLD
+
+#mpi_rank = MPI.Comm_rank(comm)
+#mpi_size = MPI.Comm_size(comm)
+
+#==============================#
+#== get atom/basis set pairs ==# 
+#==============================#
+
+pairs = Vector{String}([])
+status_bsed = h5open("../records/bsed.h5","r") do bsed
+  #== loop over atoms ==#
+  for symbol in names(bsed)
+    #== loop over basis sets in atom ==#
+    for basis in names(bsed["$symbol"])
+      #== add atom/basis-set pair to pair list ==#
+      pair = "$symbol/$basis"
+      push!(pairs, pair)
+    end
+  end
+end
+
+status_eatom = h5open("eatom.h5","w") do eatom
+  #if mpi_rank == 0
+    flush(eatom)
+  #end
+  
+  #symbol = "O" 
+  #basis = "6-31G" 
+  #pair = "$symbol/$basis"
+  
+  #MPI.Barrier(comm)
+  for (pair_idx, pair) in enumerate(pairs)
+    #if mpi_rank != (pair_idx-1)%mpi_size continue end
+
+    pair_regex = match(r"(.*)/(.*)", pair)
+    symbol = pair_regex.captures[1]
+    basis = pair_regex.captures[2]
+ 
+    println("$symbol/$basis")
+    
+    try 
+      geom = psi4.geometry("""
+        $symbol  0.0 0.0 0.0         
+        symmetry=c1
+      """)
+      
+      options = Dict(
+        "REFERENCE" => "ROHF",
+        "BASIS" => basis, 
+        "SCF_TYPE"  => "PK",
+        "GUESS" => "SAD",
+	      "PUREAM" => false,
+        #"SAD_SCF_TYPE" => "DIRECT",
+        "E_CONVERGENCE" => 1e-10,
+        "D_CONVERGENCE" => 1e-10,
+        #"MAXITER" => 1,
+        "FAIL_ON_MAXITER" => false
+      )
+      psi4.set_options(options)
+
+      name = "scf/"*options["BASIS"]
+      scf_e, scf_wfn = psi4.energy(name, molecule=geom, return_wfn=true)
+      
+      write(eatom, pair, scf_e)
+    catch e                                                                       
+      bt = catch_backtrace()                                                      
+      msg = sprint(showerror, e, bt)                                              
+      println(msg)       
+      continue
+    end
+  end
+end
+
+#MPI.Finalize()
